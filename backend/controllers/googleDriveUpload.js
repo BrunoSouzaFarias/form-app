1a4a235376f3f0f45e69d927c581147311158a74
Bruno de Souza Farias <108137122+BrunoSouzaFarias@users.noreply.github.com>
2024-09-14 00:20:04 -0300

    Api criada e configurada no serverless

--- backend/controllers/googleDriveUpload.js
+++ backend/controllers/googleDriveUpload.js
@@ -1,17 +1,19 @@
-// googleDriveUpload.js
 const fs = require('fs');
 const { google } = require('googleapis');
 
 async function uploadFile(filePath) {
   const auth = new google.auth.GoogleAuth({
-    keyFile: 'config/credentials.json', // Atualize o caminho aqui
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
     scopes: 'https://www.googleapis.com/auth/drive.file',
   });
 
   const drive = google.drive({ version: 'v3', auth });
   const fileMetadata = {
     name: 'data.csv',
-    parents: ['https://drive.google.com/drive/folders/1aacnUpI5hjsLOiSDemeqYjknfNuJQusD'] // Substitua com o ID da pasta no Google Drive
+    parents: ['1aacnUpI5hjsLOiSDemeqYjknfNuJQusD'] // Substitua com o ID da pasta correta
   };
   const media = {
     mimeType: 'text/csv',
